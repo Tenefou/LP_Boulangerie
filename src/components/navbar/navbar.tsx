d62e8bc8fb898d316407baf982cf5685e0f1c7d4
Tenefou <gaspar.rg69@gmail.com>
2025-04-04 19:37:41 +0200

    First commit

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar">
+      <div>
+        <h3>ACCEUIL</h3>
+      </div>
+      <div>
+        <h3>Acceuil</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
