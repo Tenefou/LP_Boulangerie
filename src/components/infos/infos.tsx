04d283a529f3d5ededb6b7e6554d5fd294a5838b
Tenefou <gaspar.rg69@gmail.com>
2025-04-08 01:30:58 +0200

    infos fini

--- src/components/infos/infos.tsx
+++ src/components/infos/infos.tsx
@@ -6,32 +6,53 @@ const Infos: React.FC = () => {
     <div className="infos">
       <div>
         <h2>HORAIRES :</h2>
-        <h3 className="infos__container">
-          Lundi: 8h - 16h
-          <br />
-          Mardi: 6h - 20h
-          <br />
-          Mercredi: 6h - 20h
-          <br />
-          Jeudi: 6h - 20h
-          <br />
-          Vendredi: 6h - 20h
-          <br />
-          Samedi: 8h - 16h
-          <br />
-          Dimanche: 8h - 12h
-        </h3>
+        <div className="infos__container">
+          <h3>
+            Lundi:
+            <br />
+            Mardi:
+            <br />
+            Mercredi:
+            <br />
+            Jeudi:
+            <br />
+            Vendredi:
+            <br />
+            Samedi:
+            <br />
+            Dimanche:
+          </h3>
+          <h3 className="infos__text">
+            8h - 16h
+            <br />
+            6h - 20h
+            <br />
+            6h - 20h
+            <br />
+            6h - 20h
+            <br />
+            6h - 20h
+            <br />
+            8h - 16h
+            <br />
+            8h - 12h
+          </h3>
+        </div>
       </div>
       <div>
         <h2>LOCALISATION :</h2>
-        <h3 className="infos__container">
+        <h3 className="infos__container infos__text">
           1 Rue du Château d'Eau,
           <br />
           11320 Soupex
         </h3>
       </div>
       <div>
-        <img src="" alt="carte" />
+        <img
+          style={{ width: "300px", borderRadius: "15px" }}
+          src="public/soupex.png"
+          alt="carte"
+        />
       </div>
     </div>
   );
